806311ab4db1ac217a1e21a63f0ccab1547e7657
Sahil <ss6611786@gmail.com>
2023-11-21 12:00:45 +0530

    first commit in native projects

---
+++ App.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { View,Text} from 'react-native';
+
+
+function App(): JSX.Element {
+ 
+ 
+
+  return (
+     <View>
+  <Text style={{fontSize:20}}>Hello react native i have finally achieve you</Text>
+</View>
+  );
+}
+
+export default App;
