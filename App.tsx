d0a5d4ff8e51d6db49263ee7687d76cf55734cc9
Sahil <ss6611786@gmail.com>
2023-12-21 21:59:01 +0530

    created a text-box and button

--- App.tsx
+++ App.tsx
@@ -1,27 +1,42 @@
 
-import React from 'react';
-import { View,Text,Button, FlatList} from 'react-native';
+import React, { useState } from 'react';
+import { View,Text,Button, FlatList, StyleSheet, TextInput} from 'react-native';
 
 
 function App(): JSX.Element {
-  const arraydata = [
-    { id: 1, name: "super" },
-    { id: 2, name: "dry" },
-    { id: 3, name: "hot" },
-    { id: 4, name: "cool" },
-    { id: 6, name: "not" },
-    { id: 0, name: "use" }
-    
- ]
- 
+  const [val, setVal] = useState('');
+  const [store, setStore] = useState(' ');
+  const showStore = () => {
+    setStore(val);
+ }
 
   return (
-     <View>
-      <Text style={{ fontSize: 20 }}>Hello react native i have finally achieve you</Text>
-        
-      <Button title='press me'></Button>
+    <View>
+      <Text style={styles.container}>This is start of my app building</Text>
+      <TextInput
+        style={styles.textInput}
+        value={val}
+        onChangeText={(e)=>setVal(e.target.value)}
+      />
+      <View>
+        <Text>{store}</Text>
+      </View>
+      <Button title='press me'
+      onPress={showStore}
+      />
     </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    textAlign:'center'
+  },
+  textInput: {
+    borderColor:'red',
+    borderWidth: 2,
+    color:'#717171',
+  }
+})
