b9ae61345da9ecf13f7aa5e66d9b459c6863f532
Sahil <ss6611786@gmail.com>
2023-12-25 15:17:57 +0530

    todo app is complete basic functionality

--- App.tsx
+++ App.tsx
@@ -1,29 +1,61 @@
-
-import React, { useState } from 'react';
-import { View,Text,Button, FlatList, StyleSheet, TextInput} from 'react-native';
-
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Alert,
+} from 'react-native';
 
 function App(): JSX.Element {
   const [val, setVal] = useState('');
-  const [store, setStore] = useState(' ');
+  const [store, setStore] = useState([
+    {
+      id: 123,
+      text: 'where is my money',
+    },
+  ]);
   const showStore = () => {
-    setStore(val);
- }
-
+    if (val == '') return Alert.alert('Danger', 'Enter val for a result', [{
+      text: 'ok',
+    }]);
+    setStore(prev => [...prev, {id: Math.random() * 3, text: val}]);
+    setVal('');
+  };
+  const handleDelete = id => {
+    const updatedArray = store.filter(data => data.id != id);
+    setStore(updatedArray);
+  };
   return (
     <View>
       <Text style={styles.container}>This is start of my app building</Text>
       <TextInput
         style={styles.textInput}
         value={val}
-        onChangeText={(e)=>setVal(e.target.value)}
+        onChangeText={text => setVal(text)}
       />
-      <View>
-        <Text>{store}</Text>
+      <View style={styles.buttoncontainer}>
+        <Button title="press me" onPress={showStore} />
       </View>
-      <Button title='press me'
-      onPress={showStore}
-      />
+      <ScrollView style={styles.outputcontainer}>
+        {store &&
+          store.map(data => (
+            <Text
+              style={{
+                color: 'white',
+                backgroundColor: 'green',
+                margin: 10,
+                height: 30,
+                fontSize: 20,
+              }}
+              onPress={() => handleDelete(data.id)}>
+              {data.text}
+            </Text>
+          ))}
+      </ScrollView>
     </View>
   );
 }
@@ -32,11 +64,26 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    textAlign:'center'
+    textAlign: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
+    color: '#171717',
   },
   textInput: {
-    borderColor:'red',
+    borderColor: 'red',
     borderWidth: 2,
-    color:'#717171',
-  }
-})
+    color: '#717171',
+    marginHorizontal: 10,
+    marginVertical: 2,
+    borderRadius: 20,
+  },
+  buttoncontainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  outputcontainer: {
+    paddingLeft: 10,
+    color: 'green',
+    height:400,
+  },
+});
